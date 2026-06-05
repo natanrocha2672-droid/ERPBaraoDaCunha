b92eaf7f7ba8e8f093b7fd90509012683f1e32d0
natanrocha2672-droid <natanrocha2672@gmail.com>
2026-06-05 08:32:55 -0300

    Criar entrada da interface fiscal

---
+++ src/fiscal-entry.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import FiscalApp from './FiscalApp';
+
+createRoot(document.getElementById('root')!).render(<FiscalApp />);
